815837737ae418d353a2ee5d2227f62bb1545e65
Zz <zhuzhi2013@gmail.com>
2019-05-24 17:54:30 +0800

    Initial Commit

---
+++ Xcode10-XcodeServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode10-XcodeServer
+//
+//  Created by Zz on 2019/5/24.
+//  Copyright © 2019 Zz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Xcode10-XcodeServerTests/Xcode10_XcodeServerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Xcode10_XcodeServerTests.swift
+//  Xcode10-XcodeServerTests
+//
+//  Created by Zz on 2019/5/24.
+//  Copyright © 2019 Zz. All rights reserved.
+//
+
+import XCTest
+@testable import Xcode10_XcodeServer
+
+class Xcode10_XcodeServerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
